6190a669dba42713fa1305c380182b4a1d22e346
umer <mumarkhan383@gmail.com>
2024-08-13 16:08:42 +0500

    bannerphone

--- src/app/[lang]/(frontend)/_components/banner.tsx
+++ src/app/[lang]/(frontend)/_components/banner.tsx
@@ -121,6 +121,10 @@ const Banner = () => {
                     <div className='px-2'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg></div>
                     <input className='outline-none' type="text" placeholder='Enter Fullname' name='fullname' />
                 </div>
+                <div className='flex items-center border border-gray-300 bg-white rounded-md py-2'>
+                    <div className='px-2'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg></div>
+                    <input className='outline-none' type="number" placeholder='Enter Phone Number' name='number' />
+                </div>
             </form>
             </div>
             </div>
